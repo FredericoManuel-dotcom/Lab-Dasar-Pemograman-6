5ca36d66d0b34db03c736676456d832d082bc275
Frederico <mricoboy28@gmail.com>
2026-03-12 14:59:51 +0700

    tugas.cpp

--- ctt daan tugas/tugas.cpp
+++ ctt daan tugas/tugas.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
 
-    for(int i = 1; i <= 7; i++) {        // jumlah baris
-        for(int j = 1; j <= i; j++) {    // jumlah bintang mengikuti baris
+    for(int i = 1; i <= 7; i++) {      
+        for(int j = 1; j <= i; j++) {    
             cout << "*";
         }
         cout << endl;
     }
 
     return 0;
+
 }
